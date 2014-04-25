a66002a7abda3284ac1750e10e5d69c067a79eb5
Romain Maneschi <romain.maneschi@itkweb.com>
2014-04-25 11:11:33 +0200

    add implementation of getNextQuestion

--- app/src/viewer/ViewerService.js
+++ app/src/viewer/ViewerService.js
@@ -3,36 +3,34 @@ angular.module('app').factory('ViewerService', [
 
     'use strict';
 
+    var score = 0, currentQuestion;
+
     var QCM = {
       title: 'Mon beau QCM',
-      nextAction: 'nextQuestion'//Display answer, KnowedNextQuestion
+      nextAction: 'nextQuestion',//Display answer, KnowedNextQuestion
+      questions: [
+        {
+          id: 0,
+          title: 'titre question 1',
+          score: 1,
+          multiple: false,
+          urlImage: 'https://avatars3.githubusercontent.com/u/1492516?s=140',
+          answers: [
+            {
+              id: 0,
+              title: 'Réponse 1',
+              good: true,
+              nextQuestion: 1
+            }, {
+              id: 1,
+              title: 'Réponse 2',
+              good: false,
+              nextQuestion: 1
+            }
+          ] 
+        }
+      ]
     };
-
-    var questions = [
-      {
-        id: 0,
-        title: 'titre question 1',
-        score: 1,
-        multiple: false,
-        urlImage: 'https://avatars3.githubusercontent.com/u/1492516?s=140',
-        answers: [
-          {
-            id: 0,
-            title: 'Réponse 1',
-            good: true,
-            nextQuestion: 1
-          },
-          {
-            id: 1,
-            title: 'Réponse 2',
-            good: false,
-            nextQuestion: 1
-          }
-        ] 
-      }
-    ];
-
-    QCM.questions = questions;
 
     return { 
 
@@ -40,7 +38,34 @@ angular.module('app').factory('ViewerService', [
         return QCM.title;
       },
       getNextQuestion: function(optAnswers) {
-        return questions[0];
+        if(optAnswers !== undefined) {
+          if(_.isArray(optAnswers) === true) {
+            var goodAnswers = _.filter(currentQuestion.answers, function(answer) {
+              return answer.good === true;
+            });
+            var hasWin = _.every(optAnswers, function(answer) {
+              return _.find(currentQuestion.answers, function(goodAnswer) {
+                return answer.id === goodAnswer.id;
+              });
+            });
+            if(hasWin === true) {
+              score = score + currentQuestion.score;
+            }
+          } else {
+            var goodAnswer = _.find(currentQuestion.answers, function(answer) {
+              return answer.good === true;
+            });
+            if(goodAnswer !== undefined && optAnswers.id === goodAnswer.id) {
+              score = score + currentQuestion.score;
+            }
+          }
+          if(QCM.nextAction === 'nextQuestion') {
+            currentQuestion = QCM.questions[currentQuestion.id+1];
+          }
+        } else {
+          currentQuestion = QCM.questions[0];
+        }
+        return currentQuestion;
       }
 
     };

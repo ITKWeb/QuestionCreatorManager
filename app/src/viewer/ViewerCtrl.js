5f4bf8f5d1041fbee0c9f4837bdae7d561f540fa
Michele Scapin <michele.scapin@itkweb.com>
2014-04-25 12:10:53 +0200

    fbgbrbrbùùùù

--- app/src/viewer/ViewerCtrl.js
+++ app/src/viewer/ViewerCtrl.js
@@ -1,10 +1,36 @@
 angular.module('app').controller('ViewerCtrl', [
   '$scope', 'ViewerService',
   function($scope, viewerService) {
-
     'use strict';
 
     $scope.qcmtitle = viewerService.getTitle();
+
+    var nextAction = 'nextQuestion'; //FIXME : replace with a service call
+
+    var nextQuestion = viewerService.getNextQuestion();
+
+    $scope.questionimageurl = nextQuestion.urlImage;
+
+    $scope.questiontitle = nextQuestion.title;
+
+    $scope.answerType = 'radio';
+    if (nextQuestion.multiple) {
+      $scope.answerType = 'checkbox';  
+    }
+    
+    $scope.buttonLabel = 'NextQuestion...';
+    if (nextAction === 'nextQuestion') {
+      $scope.buttonLabel = 'Next Question';  
+    } else if(nextAction === 'displayAnswer'){
+      $scope.buttonLabel = 'Display answer';  
+    }
+
+    
+
+
+    
+
+    $scope.answers = nextQuestion.answers;
 
   }
 ]);

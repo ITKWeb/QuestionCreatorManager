b31df7efc6fd2681fa6c00842dd3689582485d38
Romain Maneschi <romain.maneschi@itkweb.com>
2014-04-25 16:08:18 +0200

    add first itk qcm shot

--- app/src/services/DataService.js
+++ app/src/services/DataService.js
@@ -260,7 +260,128 @@ angular.module('app').factory('DataService', [
       ]
     };
 
-    var QCMs = {simple: QCMSimple, multiple: QCMMultiple, displayAnswer: QCMDisplayAnswer, knowedNextQuestion: QCMKnowedNextQuestion};
+    var QCMItk = {
+      title: 'ITK',
+      nextAction: 'nextQuestion',//displayAnswer, knowedNextQuestion
+      questions: [
+        {
+          id: 0,
+          title: 'Quand a été créée ITK',
+          score: 1,
+          multiple: false,
+          urlImage: 'http://itkweb.com/wp-content/uploads/logos/logo_itk.png',
+          answers: [
+            {
+              id: 0,
+              title: '2000',
+              good: false
+            }, {
+              id: 1,
+              title: '2003',
+              good: true
+            }, {
+              id: 2,
+              title: '2005',
+              good: false
+            }
+          ] 
+        }, {
+          id: 1,
+          title: 'ITK Protect est plus connus sous le nom',
+          score: 1,
+          multiple: true,
+          urlImage: 'http://itkweb.com/wp-content/uploads/logos/logo_bulle_itk-protect.png',
+          answers: [
+            {
+              id: 0,
+              title: 'Movida',
+              good: true
+            }, {
+              id: 1,
+              title: '4P',
+              good: true
+            }, {
+              id: 2,
+              title: 'Bayer\'s solution',
+              good: false
+            }
+          ] 
+        }, {
+          id: 2,
+          title: 'ITK est présent sur',
+          score: 1,
+          multiple: true,
+          urlImage: 'http://itkweb.com/wp-content/uploads/logos/logo_itk.png',
+          answers: [
+            {
+              id: 0,
+              title: 'Internet',
+              good: true
+            }, {
+              id: 1,
+              title: 'Facebook',
+              good: true
+            }, {
+              id: 2,
+              title: 'Twitter',
+              good: true
+            }, {
+              id: 3,
+              title: 'ScoopIt',
+              good: false
+            }, {
+              id: 4,
+              title: 'Viadéo',
+              good: true
+            }
+          ] 
+        }, {
+          id: 3,
+          title: 'Qu\'est-ce que la sénescence',
+          score: 1,
+          multiple: false,
+          urlImage: 'http://media.melty.fr/article-705848-ajust_930/je-pense-que-nous-voyons-ce-qui-parait-etre.jpg',
+          answers: [
+            {
+              id: 0,
+              title: 'ça ne veut rien dire',
+              good: false
+            }, {
+              id: 1,
+              title: 'c\'est lorsqu\'on gagne une partie de pétanque 0 à 13',
+              good: false
+            }, {
+              id: 2,
+              title: 'un truc qu\'on a fait en h-day vismavie d\'aggros (ou pas)',
+              good: true
+            }
+          ] 
+        }, {
+          id: 4,
+          title: 'Qu\'est-ce que le H-Day',
+          score: 1,
+          multiple: true,
+          urlImage: 'http://itkweb.github.io/site-h-day/images/favicon.png',
+          answers: [
+            {
+              id: 0,
+              title: 'Un truc trop cool.',
+              good: true
+            }, {
+              id: 1,
+              title: 'Un truc où on apprend de nouvelles choses.',
+              good: true
+            }, {
+              id: 2,
+              title: 'Un truc où on peut faire des choses de tous les jours.',
+              good: false
+            }
+          ] 
+        }, 
+      ]
+    };
+
+    var QCMs = {simple: QCMSimple, multiple: QCMMultiple, displayAnswer: QCMDisplayAnswer, knowedNextQuestion: QCMKnowedNextQuestion, itk: QCMItk};
 
     return {
 
